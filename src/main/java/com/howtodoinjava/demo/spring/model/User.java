8a6dc26cba2d9b53e56e3ef6ac7179afdc96005f
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-05-02 14:28:20 +0530

    Upgraded to Spring 6

--- src/main/java/com/howtodoinjava/demo/spring/model/User.java
+++ src/main/java/com/howtodoinjava/demo/spring/model/User.java
@@ -1,56 +1,55 @@
 package com.howtodoinjava.demo.spring.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "TBL_USERS")
 public class User {
 
-   @Id
-   @GeneratedValue
-   @Column(name = "USER_ID")
-   private Long id;
+  @Id
+  @GeneratedValue
+  @Column(name = "USER_ID")
+  private Long id;
 
-   @Column(name = "USER_NAME")
-   @Size(max = 20, min = 3, message = "{user.name.invalid}")
-   @NotEmpty(message="Please Enter your name")
-   private String name;
+  @Column(name = "USER_NAME")
+  @Size(max = 20, min = 3, message = "{user.name.invalid}")
+  @NotEmpty(message = "Please Enter your name")
+  private String name;
 
-   @Column(name = "USER_EMAIL", unique = true)
-   @Email(message = "{user.email.invalid}")
-   @NotEmpty(message="Please Enter your email")
-   private String email;
+  @Column(name = "USER_EMAIL")
+  @Email(message = "{user.email.invalid}")
+  @NotEmpty(message = "Please Enter your email")
+  private String email;
 
-   public Long getId() {
-      return id;
-   }
+  public Long getId() {
+    return id;
+  }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-   public String getName() {
-      return name;
-   }
+  public String getName() {
+    return name;
+  }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-   public String getEmail() {
-      return email;
-   }
+  public String getEmail() {
+    return email;
+  }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
