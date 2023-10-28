c7cad5e14f904c93f14a51ba5fba8b4c9a11486e
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-10-28 21:31:56 +0530

    Version Update

---
+++ spring-boot-mvc-hello-world/src/main/java/com/howtodoinjava/app/model/Item.java
@@ -0,0 +1,16 @@
+package com.howtodoinjava.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Item {
+
+  Long id;
+  String name;
+
+  public Item(String name) {
+    this.name = name;
+  }
+}
