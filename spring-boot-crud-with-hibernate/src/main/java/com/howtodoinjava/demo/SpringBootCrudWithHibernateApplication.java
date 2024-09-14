128c89943b5806f72f96aef74162f805cf2f9cdd
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-09-15 02:41:21 +0530

    Crud example

---
+++ spring-boot-crud-with-hibernate/src/main/java/com/howtodoinjava/demo/SpringBootCrudWithHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithHibernateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootCrudWithHibernateApplication.class, args);
+  }
+}
